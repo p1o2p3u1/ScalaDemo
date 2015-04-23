558a393df953d59ddac89b83769d9101d0c58f57
TingWu <sdfiwuting@gmail.com>
2015-04-24 06:05:34 +0800

    test

--- src/main/scala/demo/host/ConsoleApp.scala
+++ src/main/scala/demo/host/ConsoleApp.scala
@@ -1,8 +1,18 @@
 package demo.host
 
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.stat.Statistics
+import org.apache.spark.rdd.RDD
+
 object ConsoleApp extends App{
   System.setProperty("hadoop.home.dir", "C:\\Users\\WU\\Downloads\\hadoop")
   System.setProperty("HADOOP_USER_NAME", "root")
   System.setProperty("hive.metastore.uris","thrift://n0.ccntgrid.zju.edu:9083")
 
+  val sc = new SparkContext("local", "test")
+
+  val x: RDD[Double] = sc.parallelize(1 to 10).map(_.toDouble)
+  val y: RDD[Double] = sc.parallelize(11 to 20).map(_.toDouble)
+  val corr: Double = Statistics.corr(x, y)
+  println(corr)
 }
