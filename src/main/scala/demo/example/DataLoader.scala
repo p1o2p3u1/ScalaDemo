73218efc5b3ec17dbbd7e950262ab86542d1f838
TingWu <sdfiwuting@gmail.com>
2015-04-24 10:27:48 +0800

    test

---
+++ src/main/scala/demo/example/DataLoader.scala
@@ -0,0 +1,10 @@
+package demo.example
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+class DataLoader {
+
+  private val sc = new SparkConf().setMaster("spark://n0.ccntgrid.zju.edu:7077")
+
+
+}
--- src/main/scala/demo/host/ConsoleApp.scala
+++ src/main/scala/demo/host/ConsoleApp.scala
@@ -1,6 +1,6 @@
 package demo.host
 
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.stat.Statistics
 import org.apache.spark.rdd.RDD
 
@@ -8,8 +8,9 @@ object ConsoleApp extends App{
   System.setProperty("hadoop.home.dir", "C:\\Users\\WU\\Downloads\\hadoop")
   System.setProperty("HADOOP_USER_NAME", "root")
   System.setProperty("hive.metastore.uris","thrift://n0.ccntgrid.zju.edu:9083")
-
-  val sc = new SparkContext("local", "test")
+  System.setProperty("SPARK_MASTER_IP", "10.214.0.172")
+  val conf = new SparkConf().setMaster("spark://10.214.0.172:7077").setAppName("SparkDemo")
+  val sc = new SparkContext(conf)
 
   val x: RDD[Double] = sc.parallelize(1 to 10).map(_.toDouble)
   val y: RDD[Double] = sc.parallelize(11 to 20).map(_.toDouble)
